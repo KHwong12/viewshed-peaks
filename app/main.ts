c470e6b63f6cf35b03939994ff16d80c0ab05555
Kenneth Wong <khwongk12@gmail.com>
2021-06-30 00:46:23 +0800

    feat: Display scene view of map

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,30 @@
 import EsriMap from "esri/Map";
-import MapView from "esri/views/MapView";
+import SceneView from "esri/views/SceneView";
+import ElevationLayer from "esri/layers/ElevationLayer";
 
 const map = new EsriMap({
-  basemap: "streets"
+  basemap: "satellite"
 });
 
-const view = new MapView({
-  map: map,
+// Custom elevation service
+var elevLyr = new ElevationLayer({
+  url: "https://tiles.arcgis.com/tiles/6j1KwZfY2fZrfNMR/arcgis/rest/services/HK_DTM/ImageServer"
+});
+
+// Add elevation layer to the map's ground.
+map.ground.layers.add(elevLyr);
+
+const view = new SceneView({
   container: "viewDiv",
-  center: [114.1, 22.4],
-  zoom: 12
+  map: map,
+  camera: {
+    // autocasts as new Camera()
+    position: {
+      x: 114.2,
+      y: 22.223,
+      z: 5000
+    },
+    tilt: 60,
+    heading: 345
+  }
 });
