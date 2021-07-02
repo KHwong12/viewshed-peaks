f84bbcddf18664018831b56fb93b72f62f8d2cca
Kenneth Wong <khwongk12@gmail.com>
2021-07-02 12:55:10 +0800

    feat: Add bookmarks and repsective camera angle

--- app/main.ts
+++ app/main.ts
@@ -159,3 +159,59 @@ function showClickedLocation(event) {
   graphicsLayer.add(selectedLocationGraphic);
 };
 
+/* bookmarks */
+
+const bookmarks = {
+  VICTORIA_HARBOUR: {
+    position: {
+      x: 114.20,
+      y: 22.223,
+      z: 5000,
+      spatialReference: 4326
+    },
+    heading: 345,
+    tilt: 60
+  },
+  VICTORIA_PEAK: {
+    position: {
+      x: 114.138,
+      y: 22.26,
+      z: 1500,
+      spatialReference: 4326
+    },
+    heading: 20,
+    tilt: 70
+  },
+  KOWLOON_PEAKS: {
+    position: {
+      x: 114.253,
+      y: 22.356,
+      z: 3000,
+      spatialReference: 4326
+    },
+    heading: 260,
+    tilt: 60
+  },
+  TAI_MO_SHAN: {
+    position: {
+      x: 114.09,
+      y: 22.44,
+      z: 3000,
+      spatialReference: 4326
+    },
+    heading: 140,
+    tilt: 75
+  },
+  SHARP_PEAK: {
+    position: {
+      x: 114.429,
+      y: 22.44,
+      z: 3000,
+      spatialReference: 4326
+    },
+    heading: 250,
+    tilt: 60
+  }
+};
+
+
