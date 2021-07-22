d01398a2f9d4967f1b7fbaef94542b2f10ca6554
Kenneth Wong <khwongk12@gmail.com>
2021-07-22 16:44:31 +0800

    feat: Add table header

--- app/utils.ts
+++ app/utils.ts
@@ -7,5 +7,24 @@ fetch(weatherAPI)
   .then(res => res.json())
   .then(response => {
     console.log(response);
+
+    const visibilityTable = document.createElement("table");
+    visibilityTable.className = "table";
+
+    // Add header row
+    let row = visibilityTable.insertRow(-1);
+
+    // i = 1 to omit the first column of json (i.e. Date time)
+    for (let j = 1; j < response.fields.length; j++) {
+      let headerCell = document.createElement("th");
+
+      headerCell.innerHTML = response.fields[j];
+      row.appendChild(headerCell);
+    }
+
+    console.log(visibilityTable);
+
+    // Add the html table to the div
+    document.getElementById("visibilityDiv").appendChild(visibilityTable);
   })
   .catch(err => console.error(err));
