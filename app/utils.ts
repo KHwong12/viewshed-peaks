79b7063b98a13152199fda3d14733aa76a6e6884
Kenneth Wong <khwongk12@gmail.com>
2021-07-22 16:42:31 +0800

    feat: Fetch data from HKO API

--- app/utils.ts
+++ app/utils.ts
@@ -1,1 +1,11 @@
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const weatherAPI = "https://data.weather.gov.hk/weatherAPI/opendata/opendata.php?dataType=LTMV&lang=en&rformat=json";
+
+// http://zetcode.com/javascript/jsonurl/
+fetch(weatherAPI)
+  .then(res => res.json())
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => console.error(err));
